b93ff49a7ed59abd6e43c01c932fd27c6bfb7f07
SardarArslan <82857336+SardarArslan@users.noreply.github.com>
2023-08-08 08:45:08 +0500

    Update download_HF_Question_Generation_summarization.py

--- download_HF_Question_Generation_summarization.py
+++ download_HF_Question_Generation_summarization.py
@@ -1,9 +1,9 @@
-
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 tokenizer = AutoTokenizer.from_pretrained("Sardar/sql-model-101", use_auth_token=True)
-model = AutoModelForSeq2SeqLM.from_pretrained("Sardar/sql-model-101",return_dict=True,torch_dtype=torch.float16, device_map='cpu', use_auth_token=True)
+model = AutoModelForCausalLM.from_pretrained("Sardar/sql-model-101",return_dict=True,torch_dtype=torch.float16, device_map='cpu', use_auth_token=True)
 
 
 model.save_pretrained('./sql')
